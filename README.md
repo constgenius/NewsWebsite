ee436cc30a86f9d718c8ee46631796be95be0674
robin8778 <robin.007660@gmail.com>
2024-02-04 16:04:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# News Website using only HTML, CSS and Javascript | Javascript Project
+
+Creating a News Website using only HTML, CSS, and JavaScript is an excellent project for web development enthusiasts. This tutorial guides users through the process of building a dynamic and visually appealing news platform without relying on external frameworks or libraries. Using HTML for content structure, CSS for styling, and JavaScript for interactivity, learners can understand how to fetch and display news articles dynamically.
+
+This project provides practical experience in working with APIs, handling DOM manipulation, and creating a responsive layout. By incorporating these fundamental web technologies, developers can gain a comprehensive understanding of building a functional News Website from scratch. It's an ideal learning opportunity for those looking to hone their skills in front-end web development.
+
+![News Website](images/NewsWebsite.png)
---
+++ script.js
@@ -0,0 +1,64 @@
+const API_KEY = "0ea2bdb2e0714ed0a010339f866ae4b0";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("Technology"));
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cardscontainer");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    })
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = `${article.title.slice(0, 60)}...`;
+    newsDesc.innerHTML = `${article.description.slice(0, 150)}...`;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    })
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+})
